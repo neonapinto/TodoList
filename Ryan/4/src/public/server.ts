e7a4fc1461c219cd994dde7fa3f40d3dfb3ed339
TheInvoker <rydsouza82@gmail.com>
2022-08-02 16:11:00 -0400

    Update server.ts

--- Ryan/4/src/public/server.ts
+++ Ryan/4/src/public/server.ts
@@ -12,8 +12,8 @@ export default class ServerStorage implements Storage {
         this.#name = name;
     }
 
-    #GetURL(endpoints:string[], params:params) {
-        const url = new URL([window.location.origin, ...endpoints].join("/"));
+    #GetURL(endpoint:string, params:params) {
+        const url = new URL(endpoint, window.location.origin);
         for (const k in params) { 
             url.searchParams.append(k, params[k]); 
         }
@@ -21,15 +21,15 @@ export default class ServerStorage implements Storage {
     }
 
     async Init() {
-        await fetch("/init?name=" + encodeURIComponent(this.#name));
+        await fetch(this.#GetURL("/init", {name:this.#name}));
     }
 
     async GetAllItems():Promise<Array<Row>> {
-        return await (await fetch(this.#GetURL(["item"], {name:this.#name}))).json();
+        return await (await fetch(this.#GetURL("/item", {name:this.#name}))).json();
     }
 
     async AddItem(value:string) {
-        await fetch(this.#GetURL(["item"], {name:this.#name}), {
+        await fetch(this.#GetURL("/item", {name:this.#name}), {
             method:"POST",
             body:JSON.stringify({value}),
             headers:{
@@ -39,7 +39,7 @@ export default class ServerStorage implements Storage {
     }
 
     async UpdateItem(id:number, value:string) {
-        await fetch(this.#GetURL(["item"], {id:id.toString(), name:this.#name}), {
+        await fetch(this.#GetURL("/item", {id:id.toString(), name:this.#name}), {
             method:"PUT",
             body:JSON.stringify({value}),
             headers:{
@@ -49,13 +49,13 @@ export default class ServerStorage implements Storage {
     }
 
     async RemoveItem(id:number) {
-        await fetch(this.#GetURL(["item"], {id:id.toString(), name:this.#name}), {
+        await fetch(this.#GetURL("/item", {id:id.toString(), name:this.#name}), {
             method:"DELETE"
         });
     }
 
     async RemoveAllItems() {
-        await fetch(this.#GetURL(["item"], {name:this.#name}), {
+        await fetch(this.#GetURL("/item", {name:this.#name}), {
             method:"DELETE"
         });
     }
